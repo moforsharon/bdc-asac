b85bdda3c12557e5a81f76bee1c649e558d353bf
moforsharon <moforsharon55@gmail.com>
2024-02-22 15:06:19 +0100

    made some modifications

--- api/createInsuranceCoupon.js
+++ api/createInsuranceCoupon.js
@@ -1,10 +1,25 @@
+// import axios from 'axios';
+
+// export default async function handler(req, res) {
+//     const apiUrl = 'https://afriscience-59aa5aca58b6.herokuapp.com/api/create_insurance_product/';
+
+//     try {
+//         const response = await axios.post(apiUrl, req.body);
+//         res.status(200).json(response.data);
+//     } catch (error) {
+//         console.error('Error proxying request:', error);
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// }
+
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    const apiUrl = 'https://afriscience-59aa5aca58b6.herokuapp.com/api/create_insurance_product/';
+    const { name, product_type, product_price, product_validity } = req.query;
+    const apiUrl = `https://afriscience-59aa5aca58b6.herokuapp.com/api/create_insurance_product/?name=${name}&product_type=${product_type}&product_price=${product_price}&product_validity=${product_validity}`;
 
     try {
-        const response = await axios.post(apiUrl, req.body);
+        const response = await axios.get(apiUrl);
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error proxying request:', error);
